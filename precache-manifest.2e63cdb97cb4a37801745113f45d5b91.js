a32341eca800ac8d2e9b84cf826a57ac1210d99d
unknown <sajjaadfrancis@gmail.com>
2020-07-26 21:28:20 +0200

    Updates

---
+++ precache-manifest.2e63cdb97cb4a37801745113f45d5b91.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7ab5937ce4f9df0ddb87aaa2e34e5ee6",
+    "url": "/it-logger/index.html"
+  },
+  {
+    "revision": "7cf2e41f3efa52a7b29a",
+    "url": "/it-logger/static/css/2.36020323.chunk.css"
+  },
+  {
+    "revision": "194be256249d8f6a7850",
+    "url": "/it-logger/static/css/main.36497136.chunk.css"
+  },
+  {
+    "revision": "7cf2e41f3efa52a7b29a",
+    "url": "/it-logger/static/js/2.668ff7c0.chunk.js"
+  },
+  {
+    "revision": "494382bc2e1fa58818bf1f9041d8a9bc",
+    "url": "/it-logger/static/js/2.668ff7c0.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "194be256249d8f6a7850",
+    "url": "/it-logger/static/js/main.b085cb1f.chunk.js"
+  },
+  {
+    "revision": "58ab813052182fdf6b64",
+    "url": "/it-logger/static/js/runtime-main.6e8cc9b3.js"
+  }
+]);
